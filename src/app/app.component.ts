e4f09a07a0c1028a60241d2663d38f29a5a056ca
testgitconfig <testgitconfig>
2017-09-12 13:16:31 +0700

    munuToggle, nav, NavController

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,50 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { HomePage } from '../pages/home/home';
-//import { LoginPage } from '../pages/login/login';
+ import { HomePage } from '../pages/home/home';
+// import { DetailPage } from '../pages/detail/detail';
+ import { ListPage } from '../pages/list/list';
+// import { LoginPage } from '../pages/login/login';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
+  @ViewChild(Nav) nav: Nav;
   rootPage:any = HomePage;
   page: Array<{ title: string, Component: any}>
+//  page: [{
+//   title: 'detail',
+//   Component: DetailPage  
+//  },
+//  {
+//   title: 'list',
+//   Component: ListPage  
+//  }]
+  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
+    this.initializeApp();
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
-    platform.ready().then(() => {
+    this.page = [
+      { title: 'home' , Component: HomePage },
+      { title: 'list' , Component: ListPage }
+    ];
+
+    
+  }
+
+  initializeApp(){
+    this.platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
-      splashScreen.hide();
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
     });
+  }
+
+  openPage(p){
+    this.nav.setRoot(p.Component);
   }
 }
 
